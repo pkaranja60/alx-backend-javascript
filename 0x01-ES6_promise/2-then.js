a296f8ffa6ad3d93d115ad485e00f4103b44f8bd
Peter karanja <peterkaranja60@gmail.com>
2024-01-10 01:20:06 +0300

    Update 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log("Got a response from the API");
+      return { status: 200, body: "success" };
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+      return new Error(); // Return an empty Error object for rejection
+    });
+}
