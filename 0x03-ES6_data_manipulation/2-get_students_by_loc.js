631fcd036404c13e2436c3e46007a71d73b46e51
Peter karanja <peterkaranja60@gmail.com>
2024-01-10 00:38:18 +0300

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-export default function getStudentsByLocation(studentsArray) {
+export default function getStudentsByLocation(studentsArray, city) {
   if (!Array.isArray(studentsArray)) {
     return [];
   }
