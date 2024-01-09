01823817943c1c16e081ea30407ec6a4e032199d
Peter karanja <peterkaranja60@gmail.com>
2024-01-10 00:39:20 +0300

    Update 3-get_ids_sum.js

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(studentsArray) {
+  if (!Array.isArray(studentsArray)) {
+    return 0;
+  }
+
+  return studentsArray.reduce((sum, student) => sum + student.id, 0);
+}
