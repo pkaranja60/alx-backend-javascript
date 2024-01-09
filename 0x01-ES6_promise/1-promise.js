1a7573ec88bddb671338a9e56f41ffaa5299a710
Peter karanja <peterkaranja60@gmail.com>
2024-01-10 01:18:46 +0300

    Create 1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (success) {
+          const responseData = { status: 200, body: "Success" };
+          resolve(responseData);
+        } else {
+          reject(new Error("The fake API is not working currently"));
+        }
+      }, 1000);
+    });
+  }
+  
