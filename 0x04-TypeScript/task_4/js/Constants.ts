0a0df6a33ff7983f9fe462728f8ddeaec783b9ff
Peter karanja <peterkaranja60@gmail.com>
2024-02-27 05:22:28 -0600

    0x05. NodeJS Basics

---
+++ 0x04-TypeScript/task_4/js/Constants.ts
@@ -0,0 +1,10 @@
+import { Cpp, Java, React, Teacher } from "./subjects";
+
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+export const cTeacher: Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  experienceTeachingC: 10,
+};
